945fed7367be895863f5f9d75a74d6cf33fbc869
eflorentino25 <kokokaratino@gmail.com>
2026-09-02 23:25:27 -0300

    V1 funcionando

--- OO_Proj1/Form1.Designer.cs
+++ OO_Proj1/Form1.Designer.cs
@@ -28,32 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             S1_PBOX_MAIN = new PictureBox();
+            S1_LBL_XPOS = new Label();
+            S1_LBL_YPOS = new Label();
+            S1_TM_UPD = new System.Windows.Forms.Timer(components);
             ((System.ComponentModel.ISupportInitialize)S1_PBOX_MAIN).BeginInit();
             SuspendLayout();
             // 
             // S1_PBOX_MAIN
             // 
-            S1_PBOX_MAIN.Location = new Point(205, 72);
+            S1_PBOX_MAIN.BackgroundImage = (Image)resources.GetObject("S1_PBOX_MAIN.BackgroundImage");
+            S1_PBOX_MAIN.BackgroundImageLayout = ImageLayout.Stretch;
+            S1_PBOX_MAIN.Location = new Point(262, 114);
             S1_PBOX_MAIN.Name = "S1_PBOX_MAIN";
-            S1_PBOX_MAIN.Size = new Size(252, 205);
+            S1_PBOX_MAIN.Size = new Size(186, 178);
             S1_PBOX_MAIN.TabIndex = 0;
             S1_PBOX_MAIN.TabStop = false;
+            S1_PBOX_MAIN.Click += S1_PBOX_MAIN_Click;
+            // 
+            // S1_LBL_XPOS
+            // 
+            S1_LBL_XPOS.AutoSize = true;
+            S1_LBL_XPOS.Location = new Point(590, 84);
+            S1_LBL_XPOS.Name = "S1_LBL_XPOS";
+            S1_LBL_XPOS.Size = new Size(0, 15);
+            S1_LBL_XPOS.TabIndex = 1;
+            // 
+            // S1_LBL_YPOS
+            // 
+            S1_LBL_YPOS.AutoSize = true;
+            S1_LBL_YPOS.Location = new Point(590, 128);
+            S1_LBL_YPOS.Name = "S1_LBL_YPOS";
+            S1_LBL_YPOS.Size = new Size(0, 15);
+            S1_LBL_YPOS.TabIndex = 2;
+            // 
+            // S1_TM_UPD
+            // 
+            S1_TM_UPD.Enabled = true;
+            S1_TM_UPD.Tick += S1_TM_UPD_Tick;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(945, 608);
+            Controls.Add(S1_LBL_YPOS);
+            Controls.Add(S1_LBL_XPOS);
             Controls.Add(S1_PBOX_MAIN);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)S1_PBOX_MAIN).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private PictureBox S1_PBOX_MAIN;
+        private Label S1_LBL_XPOS;
+        private Label S1_LBL_YPOS;
+        private System.Windows.Forms.Timer S1_TM_UPD;
     }
 }
--- OO_Proj1/Form1.cs
+++ OO_Proj1/Form1.cs
@@ -2,9 +2,58 @@ namespace OO_Proj1
 {
     public partial class Form1 : Form
     {
+        int gv_deltax = 1,gv_deltay = 1;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void S1_PBOX_MAIN_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void S1_TM_UPD_Tick(object sender, EventArgs e)
+        {
+            S1_LBL_XPOS.Text = "X:" + S1_PBOX_MAIN.Location.X.ToString();
+            S1_LBL_YPOS.Text = "Y:" + S1_PBOX_MAIN.Location.Y.ToString();
+
+            int lv_ScreenSizeX = this.Size.Width,
+                lv_ScreenSizeY = this.Size.Height - 25;
+
+            int lv_ImageSizeX = S1_PBOX_MAIN.Size.Width,
+                lv_ImageSizeY = S1_PBOX_MAIN.Size.Height;
+
+            int lv_ImagePosX = S1_PBOX_MAIN.Location.X,
+                lv_ImagePosY = S1_PBOX_MAIN.Location.Y;
+            // (x,y) da S1_PBOX_MAIN
+
+            lv_ImagePosX = lv_ImagePosX + gv_deltax;
+            lv_ImagePosY = lv_ImagePosY + gv_deltay;
+
+            if ((lv_ImagePosX > (lv_ScreenSizeX - lv_ImageSizeX)) || (lv_ImagePosX < 0))
+            {
+                gv_deltax = -gv_deltax;
+            }
+            else
+            {
+                S1_PBOX_MAIN.Location = new Point(lv_ImagePosX, lv_ImagePosY);
+            }
+            if ((lv_ImagePosY > (lv_ScreenSizeY - lv_ImageSizeY)) || (lv_ImagePosY < 0))
+            {
+                gv_deltay = -gv_deltay;
+            }
+            else
+            {
+                S1_PBOX_MAIN.Location = new Point(lv_ImagePosX, lv_ImagePosY);
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            S1_TM_UPD.Start();
         }
     }
 }
